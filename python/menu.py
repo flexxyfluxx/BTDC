3611ca7d76dccd64a5d969b3e0bbb8bc6965f9a1
flexxyfluxx <82163962+flexxyfluxx@users.noreply.github.com>
2023-03-18 14:51:58 +0000

    fix debug mass tower placing

--- python/menu.py
+++ python/menu.py
@@ -158,7 +158,7 @@ class Menu(JMainFrame):
         for x in range(0, 96):
             for y in range(0, 54):
                 self.game.heldTower = HeldTower(1) 
-                self.game.placeTower(Vektor(x*10, y*10))
+                self.game.placeHeldTower(Vektor(x*10, y*10))
 
     def bTower4_ActionPerformed(self, _):
         # self.game.heldTower = HeldTower(3)
