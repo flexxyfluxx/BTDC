b333b230ac5c17c56e9ba077b514ae4ca1c031cd
Poseidon13856 <poseidon138562@gmail.com>
2023-03-19 13:35:05 +0100

    Update enemies.py

--- python/enemies.py
+++ python/enemies.py
@@ -7,7 +7,7 @@ from ch.aplu.jgamegrid import GGBitmap
 WEAKEST = lambda game, key, segmentIdx=0, segmentProgress=0: Enemy(
     game=game, key=key,
     dmg=1, health=1, speed=1.5,
-    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/baloon1.png"), 0.8, 0),  # TODO add proper sprite
+    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/ballon1.png"), 0.8, 0),  # TODO add proper sprite
     segmentIdx=segmentIdx, segmentProgress=segmentProgress
 )
 
