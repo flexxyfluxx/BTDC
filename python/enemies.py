8af1175ae9e1d75068d01abc09d9ec126ef4547b
fuck the industrialisation <122464332+ManAgainstIndustrialisation@users.noreply.github.com>
2023-03-19 13:30:48 +0100

    spirtes

--- python/enemies.py
+++ python/enemies.py
@@ -2,18 +2,19 @@
 
 from enemy import Enemy
 from os.path import abspath
+from ch.aplu.jgamegrid import GGBitmap
 
 WEAKEST = lambda game, key, segmentIdx=0, segmentProgress=0: Enemy(
     game=game, key=key,
     dmg=1, health=1, speed=1.5,
-    sprite=abspath("../assets/sprites/picasso.png"),  # TODO add proper sprite
+    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/ballon1.png"),0.8,0),  # TODO add proper sprite
     segmentIdx=segmentIdx, segmentProgress=segmentProgress
 )
 
 BLUE_EQ = lambda game, key, segmentIdx=0, segmentProgress=0: Enemy(
     game=game, key=key,
     dmg=2, health=1, speed=1.8,
-    sprite=abspath("../assets/sprites/sprite2.png"),  # TODO add proper sprite
+    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/ballon2.png"),0.8,0),  # TODO add proper sprite
     segmentIdx=segmentIdx, segmentProgress=segmentProgress,
     childSupplier=WEAKEST
 )
