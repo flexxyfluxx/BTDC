fb90884560a3653424616fbcf05f77df2b3d3222
flexxyfluxx <felix@vonharrach.de>
2023-03-24 13:35:22 +0100

    add rounds up to 7; add enemies up to Pink (missing sprites)

--- python/enemies.py
+++ python/enemies.py
@@ -7,14 +7,38 @@ from ch.aplu.jgamegrid import GGBitmap
 WEAKEST = lambda game, key, segmentIdx=0, segmentProgress=0: Enemy(
     game=game, key=key,
     dmg=1, health=1, speed=1.5,
-    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/ballon1.png"), 0.8, 0),  # TODO add proper sprite
+    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/ballon1.png"), 0.8, 0),
     segmentIdx=segmentIdx, segmentProgress=segmentProgress
 )
 
-BLUE_EQ = lambda game, key, segmentIdx=0, segmentProgress=0: Enemy(
+BLUE = lambda game, key, segmentIdx=0, segmentProgress=0: Enemy(
     game=game, key=key,
-    dmg=2, health=1, speed=1.8,
-    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/ballon2.png"), 0.8, 0),  # TODO add proper sprite
+    dmg=2, health=1, speed=2,
+    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/ballon2.png"), 0.8, 0),
     segmentIdx=segmentIdx, segmentProgress=segmentProgress,
     childSupplier=WEAKEST
+)
+
+GREEN = lambda game, key, segmentIdx=0, segmentProgress=0: Enemy(
+    game=game, key=key,
+    dmg=3, health=1, speed=3,
+    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/spritesprite.png"), 0.8, 0),
+    segmentIdx=segmentIdx, segmentProgress=segmentProgress,
+    childSupplier=BLUE
+)
+
+YELLOW = lambda game, key, segmentIdx=0, segmentProgress=0: Enemy(
+    game=game, key=key,
+    dmg=4, health=1, speed=5,
+    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/spritesprite.png"), 0.8, 0),
+    segmentIdx=segmentIdx, segmentProgress=segmentProgress,
+    childSupplier=GREEN
+)
+
+PINK = lambda game, key, segmentIdx=0, segmentProgress=0: Enemy(
+    game=game, key=key,
+    dmg=5, health=1, speed=8,
+    sprite=GGBitmap.getScaledImage(abspath("../assets/sprites/spritesprite.png"), 0.8, 0),
+    segmentIdx=segmentIdx, segmentProgress=segmentProgress,
+    childSupplier=YELLOW
 )
